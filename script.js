f5fe3935a77db85dbcc7e6d0f5f72ebf3a3df1d8
DIR\gabriel.de.s.barbosa <gabriel.de.s.barbosa@accenture.com>
2018-02-26 16:42:58 -0300

    no message

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-app.use(
+/*app.use(
      "/", //the URL throught which you want to access to you static content
      express.static(__dirname) //where your static content is located in your filesystem
-);
+);*/
 app.listen(3000); //the port you want to use
